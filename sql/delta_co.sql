01bfbada9762d357b31c4efb8d47e98e4533a95d
Ahad Sheriff <ahadsheriff@gmail.com>
2019-03-03 17:11:13 -0500

    test sql db

---
+++ sql/delta_co.sql
@@ -0,0 +1,152 @@
+CREATE TABLE EMPLOYEE
+(
+    ID int,
+    NAME char(255),
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY int,
+    PRIMARY KEY(ID)
+);
+
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+    )
+VALUES (
+    3,
+    'Bob',
+    'bob@delta.com'
+    'password',
+    4405
+);
+
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    2,
+    'Julia',
+    'julia@delta.com'
+    'I_love_bob'
+    2559
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    5,
+    'Alaina',
+    'alaina@delta.com'
+    'red_sox1'
+    1151
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    1,
+    'Candice',
+    'candice@delta.com'
+    'fido.is_mydog1'
+    4685
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    4
+    'Scarlet',
+    'scarlet@delta.com',
+    'telracS_2221988'
+    2350
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    6
+    'Squidward',
+    'squidward@delta.com',
+    'salty_spitoon25'
+    16000
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    7
+    'Ahad',
+    'ahad@delta.com',
+    'Password_1'
+    25000
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    8,
+    'Spongebob',
+    'spongebob@delta.com',
+    'pineapple_way'
+    1000
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    9,
+    'Patrick',
+    'patrick@delta.com',
+    'noShirtGang'
+    200
+);
+INSERT INTO EMPLOYEE (
+    ID,
+    NAME,
+    EMAIL char(255),
+    PASSWORD char(255),
+    SALARY
+)
+VALUES (
+    10,
+    'Plankton',
+    'plankton@delta.com',
+    'steal1the2formula3'
+    300
+);
+
