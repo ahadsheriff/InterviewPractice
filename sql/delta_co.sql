05948d28ac87afa3ebe686ca202160aa3551c383
Ahad Sheriff <ahadsheriff@gmail.com>
2019-03-03 17:17:50 -0500

    delte_co syntax errors

--- sql/delta_co.sql
+++ sql/delta_co.sql
@@ -18,7 +18,7 @@ INSERT INTO employees (
 VALUES (
     3,
     'Bob',
-    'bob@delta.com'
+    'bob@delta.com',
     'password',
     4405
 );
@@ -33,8 +33,8 @@ INSERT INTO employees (
 VALUES (
     2,
     'Julia',
-    'julia@delta.com'
-    'I_love_bob'
+    'julia@delta.com',
+    'I_love_bob',
     2559
 );
 INSERT INTO employees (
@@ -47,8 +47,8 @@ INSERT INTO employees (
 VALUES (
     5,
     'Alaina',
-    'alaina@delta.com'
-    'red_sox1'
+    'alaina@delta.com',
+    'red_sox1',
     1151
 );
 INSERT INTO employees (
@@ -61,8 +61,8 @@ INSERT INTO employees (
 VALUES (
     1,
     'Candice',
-    'candice@delta.com'
-    'fido.is_mydog1'
+    'candice@delta.com',
+    'fido.is_mydog1',
     4685
 );
 INSERT INTO employees (
@@ -76,7 +76,7 @@ VALUES (
     4
     'Scarlet',
     'scarlet@delta.com',
-    'telracS_2221988'
+    'telracS_2221988',
     2350
 );
 INSERT INTO employees (
@@ -90,7 +90,7 @@ VALUES (
     6
     'Squidward',
     'squidward@delta.com',
-    'salty_spitoon25'
+    'salty_spitoon25',
     16000
 );
 INSERT INTO employees (
@@ -104,7 +104,7 @@ VALUES (
     7
     'Ahad',
     'ahad@delta.com',
-    'Password_1'
+    'Password_1',
     25000
 );
 INSERT INTO employees (
@@ -118,7 +118,7 @@ VALUES (
     8,
     'Spongebob',
     'spongebob@delta.com',
-    'pineapple_way'
+    'pineapple_way',
     1000
 );
 INSERT INTO employees (
@@ -132,7 +132,7 @@ VALUES (
     9,
     'Patrick',
     'patrick@delta.com',
-    'noShirtGang'
+    'noShirtGang',
     200
 );
 INSERT INTO employees (
@@ -146,7 +146,7 @@ VALUES (
     10,
     'Plankton',
     'plankton@delta.com',
-    'steal1the2formula3'
+    'steal1the2formula3',
     300
 );
 
